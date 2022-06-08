a5e4a0d6535be70d4c4dbce92beac26bb3b4b6eb
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-06-08 07:58:18 -0300

    atualizar

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function calcularMelhorPreco(){
+  
+  //validar campos
+
+  let precoAlcool = document.getElementById('alcool').value
+  let precoGasolina = document.getElementById('gasolina').value
+
+  if( precoAlcool != "" ){
+    if(precoGasolina != "" ){
+      /*Calcular se é melhor usar álcool ou gasolina* se valorAlcool / valorGasolina >= 0.7 é melhor utilizar Gasolina * senão é melhor utilizar álcool*/
+
+      let resultado = precoAlcool / precoGasolina
+      if(resultado >= 0.7){
+        //alert("Melhor utilizar Gasolina")
+        document.getElementById('resultado').innerHTML = "Melhor utilizar Gasolina"
+      }else{
+        //alert("Melhor utilizar alcool")
+        document.getElementById('resultado').innerHTML = "Melhor utilizar Alcool"
+
+
+      }
+
+    }else{
+      alert("Preencha o preço da Gasolina")
+    }
+
+  }else{
+    alert("Preencha o preço do Álcool")
+  }
+
+}
